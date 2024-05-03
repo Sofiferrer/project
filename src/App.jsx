24853fc06e4218bc4443501b45946bd67f7a8d0a
sofia <sofia@blockinar.com>
2024-05-03 00:00:16 -0300

    initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./App.css";
+import Home from "./views/Home";
+import Login from "./views/Login";
 
 function App() {
   const screensState = useSelector((state) => state.screens);
   console.log(screensState);
   return (
-    <>
-      <p>latinAd</p>
-    </>
+    <div className="wrapper">
+      <Router>
+        <Routes>
+          <Route exact path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
