e1fb73627f7407369cc16a1d19c6ea1b0e55de4c
sofia <sofia@blockinar.com>
2024-05-03 00:00:58 -0300

    login working with redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import screensReducer from "../features/screens/screenSlice";
+import authReducer from "../features/auth/authSlice";
+
 export const store = configureStore({
   reducer: {
     screens: screensReducer,
+    auth: authReducer,
   },
 });
