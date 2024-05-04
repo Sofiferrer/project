7bf39d85eea91f9af8a4cc233dc55e1d6c475ac6
sofia <sofia@blockinar.com>
2024-05-04 19:13:05 -0300

    import useDispatch

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Button } from "antd";
 import { logout } from "../../features/auth/authSlice";
 import "./Navbar.css";
+import { useDispatch } from "react-redux";
 
 export default function Navbar() {
+  const dispatch = useDispatch();
+
   const logOut = () => {
     dispatch(logout()).then((result) => console.log("logout", result));
   };
