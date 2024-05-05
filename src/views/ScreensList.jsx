45baf05f824cdc066e2b8d24a258999db423edca
sofia <sofia@blockinar.com>
2024-05-05 20:36:39 -0300

    useSearchParams for url filtering, useMemo,

--- src/views/ScreensList.jsx
+++ src/views/ScreensList.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useMemo } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getAll } from "../features/screens/screenSlice";
-import { Pagination, Form, Input, Radio, Button } from "antd";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/auth/authSlice";
+import { Button } from "antd";
 import Loader from "../components/Loader/Loader";
 import Navbar from "../components/Navbar/Navbar";
+import Paginator from "../components/Paginator/Paginator";
+import Filters from "../components/Filters/Filters";
 
 export default function ScreensList() {
   const dispatch = useDispatch();
@@ -14,152 +16,124 @@ export default function ScreensList() {
   const token = useSelector(selectUser);
   const { loading } = useSelector((state) => state.screens);
 
-  const [filterForm] = Form.useForm();
-
   const [totalScreens, setTotalScreens] = useState(0);
   const [screens, setScreens] = useState([]);
   const [page, setPage] = useState(1);
+  const [reset, setReset] = useState(false);
   const [pageSize, setPageSize] = useState(10);
-  const [params, setParams] = useState({
+
+  const [queryParams, setQueryParams] = useSearchParams({
     name: "",
     type: "",
     pageSize: 10,
     offset: 0,
   });
 
-  useEffect(() => {
-    filterForm.setFieldsValue({
-      name: "",
-      type: "",
-    });
-  }, []);
+  const searchParams = useMemo(() => {
+    const name = queryParams.get("name") || "";
+    const type = queryParams.get("type") || "";
+    const pageSize = parseInt(queryParams.get("pageSize") || 10);
+    const offset = parseInt(queryParams.get("offset") || 0);
+    return {
+      name,
+      type,
+      pageSize,
+      offset,
+    };
+  }, [queryParams]);
 
   useEffect(() => {
-    fetchData(params, page);
-  }, [page, pageSize]);
+    fetchData();
+  }, [page, pageSize, searchParams]);
 
-  const handleFormSubmit = (values) => {
-    setParams({ name: values.name, type: values.type });
-    setPage(1);
-    fetchData(values, 1);
-  };
-
-  const fetchData = (values, page) => {
-    const searchParams = new URLSearchParams({
+  const handleFilters = (values) => {
+    setQueryParams({
       name: values.name || "",
       type: values.type || "",
-      pageSize,
-      offset: (page - 1) * pageSize,
+      pageSize: pageSize,
+      offset: 0,
     });
-    dispatch(getAll({ params: searchParams, token: token })).then((result) => {
-      setScreens(result.payload.data);
-      setTotalScreens(result.payload.totalCount);
+    setPage(1);
+    setReset(false);
+  };
+
+  const fetchData = () => {
+    const params = new URLSearchParams({
+      name: searchParams.name,
+      type: searchParams.type,
+      pageSize: searchParams.pageSize,
+      offset: searchParams.offset,
     });
+
+    dispatch(getAll({ params: params.toString(), token: token })).then(
+      (result) => {
+        setScreens(result.payload.data);
+        setTotalScreens(result.payload.totalCount);
+      }
+    );
   };
 
   const handlePageChange = (pageNumber, itemsPerPage) => {
     setPage(pageNumber);
     setPageSize(itemsPerPage);
+    const name = queryParams.get("name");
+    const type = queryParams.get("type");
+    setQueryParams({
+      name: name || "",
+      type: type || "",
+      pageSize: itemsPerPage,
+      offset: (pageNumber - 1) * itemsPerPage,
+    });
   };
 
   const onReset = () => {
-    filterForm.resetFields();
+    setReset(true);
     setPage(1);
-    setParams({
+    setPageSize(10);
+
+    setQueryParams({
       name: "",
       type: "",
       pageSize: 10,
       offset: 0,
     });
-    fetchData(
-      {
-        name: "",
-        type: "",
-        pageSize: 10,
-        offset: 0,
-      },
-      1
-    );
   };
 
   return (
     <>
       <Navbar />
-
       <Link to="/create"> Add Screen</Link>
-      <Form
-        form={filterForm}
-        name="filter-form"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        onFinish={handleFormSubmit}
-        autoComplete="off"
-      >
-        <Form.Item label="Name" name="name">
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Type" name="type">
-          <Radio.Group>
-            <Radio
-              value="indoor"
-              onChange={(e) =>
-                handleFormSubmit({ ...params, type: e.target.value })
-              }
-            >
-              {" "}
-              Indoor{" "}
-            </Radio>
-            <Radio
-              value="outdoor"
-              onChange={(e) =>
-                handleFormSubmit({ ...params, type: e.target.value })
-              }
-            >
-              {" "}
-              Outdoor{" "}
-            </Radio>
-          </Radio.Group>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit" loading={loading}>
-            Search
-          </Button>
-          <Button htmlType="button" onClick={onReset}>
-            Reset
-          </Button>
-        </Form.Item>
-      </Form>
+      <Filters
+        onSubmit={handleFilters}
+        params={queryParams}
+        reset={reset}
+        loading={loading}
+      ></Filters>
+      <Button htmlType="button" onClick={onReset}>
+        Reset
+      </Button>
       {loading ? (
         <Loader />
       ) : (
         <main>
-          {" "}
           {totalScreens > 0 ? (
             <ul>
-              {screens?.map((screen) => {
-                return (
-                  <li key={screen.id}>
-                    <Link to={`/screen/${screen.id}`}>{screen.name}</Link>
-                  </li>
-                );
-              })}
+              {screens?.map((screen) => (
+                <li key={screen.id}>
+                  <Link to={`/screen/${screen.id}`}>{screen.name}</Link>
+                </li>
+              ))}
             </ul>
           ) : (
             <p>No screens match the filter</p>
           )}
-          <Pagination
-            defaultCurrent={1}
-            current={page}
-            total={totalScreens}
-            hideOnSinglePage={true}
-            showSizeChanger={true}
-            pageSizeOptions={[10, 20, 50]}
-            onChange={handlePageChange}
-            onShowSizeChange={handlePageChange}
-          />
+          <Paginator
+            onSubmit={handlePageChange}
+            page={page}
+            pageSize={pageSize}
+            totalScreens={totalScreens}
+          ></Paginator>
+          <span>{totalScreens}</span>
         </main>
       )}
     </>
