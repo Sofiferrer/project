97b1c48dcfe70c6131040ab52ed5744950b175e9
sofia <sofia@blockinar.com>
2024-05-05 20:34:26 -0300

    paginator component

---
+++ src/components/Paginator/Paginator.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Pagination } from "antd";
+import "./Paginator.css";
+
+export default function Paginator({ onSubmit, page, pageSize, totalScreens }) {
+  return (
+    <>
+      <Pagination
+        defaultCurrent={1}
+        current={page}
+        total={totalScreens}
+        hideOnSinglePage={true}
+        pageSize={pageSize}
+        showSizeChanger={true}
+        pageSizeOptions={[10, 20, 50]}
+        onChange={onSubmit}
+        onShowSizeChange={onSubmit}
+      />
+    </>
+  );
+}
