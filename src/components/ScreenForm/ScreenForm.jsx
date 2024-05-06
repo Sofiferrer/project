dd91bac921954c63f5860896c733454aa018f55f
sofia <sofia@blockinar.com>
2024-05-06 20:45:39 -0300

    create and edit form styles

--- src/components/ScreenForm/ScreenForm.jsx
+++ src/components/ScreenForm/ScreenForm.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
-import { Button, Form, Input, InputNumber, message, Radio } from "antd";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Button, Form, Input, InputNumber, Radio, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
-export default function ScreenForm({ onSubmit, initialValues, disabled }) {
+import "./ScreenForm.css";
+
+export default function ScreenForm({ onSubmit, initialValues }) {
   const [screenForm] = Form.useForm();
   const { loading } = useSelector((state) => state.screens);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (initialValues) {
+      console.log(initialValues);
       screenForm.setFieldsValue({
         name: initialValues.name,
         description: initialValues.description,
@@ -23,12 +29,10 @@ export default function ScreenForm({ onSubmit, initialValues, disabled }) {
     <Form
       form={screenForm}
       name="screen-form"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
+      style={{ maxWidth: 600, margin: "auto" }}
       onFinish={onSubmit}
       autoComplete="off"
-      disabled={disabled}
+      layout="vertical"
     >
       <Form.Item
         label="Name"
@@ -44,7 +48,7 @@ export default function ScreenForm({ onSubmit, initialValues, disabled }) {
       </Form.Item>
 
       <Form.Item
-        label="description"
+        label="Description"
         name="description"
         rules={[
           {
@@ -56,63 +60,113 @@ export default function ScreenForm({ onSubmit, initialValues, disabled }) {
       >
         <Input.TextArea />
       </Form.Item>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: "60%" }}>
+          <div className="input-group">
+            <Form.Item
+              label="Type"
+              name="type"
+              rules={[
+                {
+                  required: true,
+                  message: "Choose a type",
+                },
+              ]}
+            >
+              <Radio.Group>
+                <Radio value="indoor"> Indoor </Radio>
+                <Radio value="outdoor"> Outdoor </Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item
+              label="Price x day"
+              name="price_per_day"
+              rules={[
+                {
+                  required: true,
+                  message: "Insert price",
+                },
+              ]}
+            >
+              <InputNumber />
+            </Form.Item>
+          </div>
+          <div className="input-group">
+            <Form.Item
+              label="Heigh"
+              name="resolution_height"
+              rules={[
+                {
+                  required: true,
+                  message: "Insert heigh",
+                },
+              ]}
+            >
+              <InputNumber />
+            </Form.Item>
 
-      <Form.Item
-        label="Price x day"
-        name="price_per_day"
-        rules={[
-          {
-            required: true,
-            message: "Insert price",
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-
-      <Form.Item
-        label="Heigh"
-        name="resolution_height"
-        rules={[
-          {
-            required: true,
-            message: "Insert heigh",
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-
-      <Form.Item
-        label="Width"
-        name="resolution_width"
-        rules={[
-          {
-            required: true,
-            message: "Insert width",
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item
-        label="Type"
-        name="type"
-        rules={[
-          {
-            required: true,
-            message: "Choose a type",
-          },
-        ]}
-      >
-        <Radio.Group>
-          <Radio value="indoor"> Indoor </Radio>
-          <Radio value="outdoor"> Outdoor </Radio>
-        </Radio.Group>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit" loading={loading}>
+            <Form.Item
+              label="Width"
+              name="resolution_width"
+              rules={[
+                {
+                  required: true,
+                  message: "Insert width",
+                },
+              ]}
+            >
+              <InputNumber />
+            </Form.Item>
+          </div>
+        </div>
+        {initialValues ? (
+          <div
+            style={{
+              width: "40%",
+              display: "flex",
+              justifyContent: "end",
+            }}
+          >
+            <img
+              alt="screen picture"
+              src={`${initialValues.picture_url}`}
+              style={{ width: "auto", height: "25vh" }}
+            />
+          </div>
+        ) : (
+          <div
+            style={{
+              width: "40%",
+              display: "flex",
+              justifyContent: "end",
+              alignItems: "center",
+            }}
+          >
+            <Form.Item label="Picture" disabled>
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                disabled
+              >
+                <UploadOutlined />
+              </Upload>
+            </Form.Item>
+          </div>
+        )}
+      </div>
+      <Form.Item style={{ marginTop: "30px" }}>
+        <Button
+          size="large"
+          type="secondary"
+          htmlType="button"
+          loading={loading}
+          onClick={() => navigate("/latinAd-react/screens")}
+        >
+          Cancel
+        </Button>
+        <Button size="large" type="primary" htmlType="submit" loading={loading}>
           {initialValues ? "Save" : "Create"}
         </Button>
       </Form.Item>
