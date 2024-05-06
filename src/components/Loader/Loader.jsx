e177f8b98debb563ac6cdcfe483e1d9d032653d1
sofia <sofia@blockinar.com>
2024-05-06 20:43:58 -0300

    loader styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Loader.css";
 
 export default function Loader() {
-  return <div className="loader"></div>;
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
 }
