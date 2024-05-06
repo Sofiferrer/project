30ffce48553c16e8962886ba2b6d7f613e293600
sofia <sofia@blockinar.com>
2024-05-06 20:43:13 -0300

    add btn component

---
+++ src/components/AddBtn/AddBtn.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FileAddOutlined } from "@ant-design/icons";
+
+import "./AddBtn.css";
+
+export default function AddBtn() {
+  return (
+    <Link to="/latinAd-react/create" className="add-btn">
+      {" "}
+      <FileAddOutlined />
+      New
+    </Link>
+  );
+}
